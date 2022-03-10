1b8cedadafb22402a5218baa32541b6d4a61bcaa
Jason Peters <ptasdevz@gmail.com>
2022-03-10 18:11:39 -0400

    add request logger

--- starter/app.js
+++ starter/app.js
@@ -5,15 +5,25 @@ const connectDB = require('./db/connect');
 require('dotenv').config();
 const notFound = require('./middleware/not-found');
 const errorhandler = require('./middleware/error-handler');
-
+const path = require('path');
+const morgan = require('morgan');
+const rfs = require('rotating-file-stream');
 
 //middleware
-
 app.use(express.static('./public'));
 app.use(express.json());
+//log all request using daily files to access.log
+app.use(morgan('combined', {
+    stream: rfs.createStream('access.log',
+        { interval: '1d', path: path.join(__dirname, 'logs') }, { flags: 'a' })
+
+}));
 app.use('/api/v1/tasks', tasks);
 app.use(notFound);
 app.use(errorhandler);
+
+
+
 
 const port = process.env.PORT || 3000
 
