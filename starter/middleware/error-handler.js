772e80a552a800fa5cdf3c828aa4db941f1018d0
Jason Peters <ptasdevz@gmail.com>
2022-03-10 17:31:33 -0400

    code clean up

--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -1,7 +1,6 @@
 const { CustomAPIError } = require("../errors/custom-error");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    console.log(err)
     if ( err instanceof CustomAPIError) return res.status(err.statusCode).json({msg:err.message});
     return res.status(500).json({msg:'An internal error has occured'});
 }
